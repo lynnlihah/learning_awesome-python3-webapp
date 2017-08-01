f73f71d6fa3e79cfa2fceea4d7f29fc10c1b0a0b
lizhufeng <lizhufeng@zdns.cn>
2017-08-01 17:17:30 +0800

    edit orm.py

--- www/app.py
+++ www/app.py
@@ -1,11 +1,17 @@
 '''web app 骨架'''
+'''由于Web框架使用了基于asyncio的aiohttp，这是基于协程的异步模型。在协程中，不能调用普通的同步
+IO操作，因为所有用户都是由一个线程服务的，协程的执行速度必须非常快，才能处理大量用户的请求。而
+耗时的IO操作不能在协程中以同步的方式调用，否则，等待一个IO操作时，系统无法响应任何其他用户。'''
+
+# 由于我们的Web App建立在asyncio的基础上，因此用aiohttp写一个基本的app.py：
 import logging; logging.basicConfig(level=logging.INFO)
 import asyncio, os, json, time
 from datetime import datetime
 from aiohttp import web
 
 def index(request):
-    return web.Response(body=b'<h1>Awesome</h1>')
+    return web.Response(body=b'<h1>Awesome</h1>',content_type='text/html')
+    # return web.Response(body=b'<h1>Awesome</h1>') 不填写 content_type 会自动变成下载页面
 
 @asyncio.coroutine
 def init(loop):
--- www/orm.py
+++ www/orm.py
@@ -1,40 +1,184 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
+# 封装常用的SELECT、INSERT、UPDATE和DELETE操作用函数
+# aiomysql为MySQL数据库提供了异步IO的驱动。
+# 实例代码原址： https://github.com/michaelliao/awesome-python3-webapp/blob/day-03/www/orm.py
 import asyncio, logging
-
 import aiomysql
 
 def log(sql, args=()):
     logging.info('SQL: %s' % sql)
-@asyncio.coroutine
-def create_pool(loop, **kw):
+# 我们需要创建一个全局的连接池，每个HTTP请求都可以从连接
+# 池中直接获取数据库连接。使用连接池的好处是不必频繁地打开和关闭数据库连接，而是能复用就尽量复用。
+# 创建连接池
+# 连接池由全局变量__pool存储，缺省情况下将编码设置为utf8，自动提交事务：
+async def create_pool(loop, **kw):
     logging.info('create database connection pool...')
     global __pool
-    __pool = yield from aiomysql.create_pool(
-        host = kw.get('host', 'localhost'),
-        port = kw.get('port', 3306),
-        user = kw['user'],
-        password = kw['password'],
-        db = kw['db'],
-        charset = kw.get('charset','utf8'),
-        autocommit = kw.get('autocommit', True),
-        maxsize = kw.get('maxsize', 10),
-        minsize = kw.get('minsize', 1),
-        loop = loop
+    __pool = await aiomysql.create_pool(
+        host=kw.get('host', 'localhost'),
+        port=kw.get('port', 3306),
+        user=kw['user'],
+        password=kw['password'],
+        db=kw['db'],
+        charset=kw.get('charset', 'utf8'),
+        autocommit=kw.get('autocommit', True),
+        maxsize=kw.get('maxsize', 10),
+        minsize=kw.get('minsize', 1),
+        loop=loop
     )
 
-@asyncio.coroutine
-
-def select(sql, args, size=None):
+# Select - 要执行SELECT语句，我们用select函数执行，需要传入SQL语句和SQL参数：
+# @asyncio.coroutine
+# def select(sql, args, size=None):
+# end
+async def select(sql, args, size=None):
     log(sql, args)
     global __pool
-    with ( yield from __pool) as conn:
-        cur = yield from conn.cursor(aiomysql.DictCursor)
-        yield from cur.execute(sql.replace('?','%s'), args or ())
-        if size:
-            rs = yield from cur.fetchmany(size)
-        else:
-            rs = yield from cur.fetchall()
-        yield from cur.close()
+    async with __pool.get() as conn:
+        async with conn.cursor(aiomysql.DictCursor) as cur:
+            # SQL语句的占位符是?，而MySQL的占位符是%s，select()函数在内部自动替换。
+            # 注意要始终坚持使用带参数的SQL，而不是自己拼接SQL字符串，这样可以防止SQL注入攻击。
+            # 注意到yield from将调用一个子协程（也就是在一个协程中调用另一个协程）并直接获得子协程的返回结果。
+            await cur.execute(sql.replace('?', '%s'), args or ())
+            # 如果传入size参数，就通过fetchmany()获取最多指定数量的记录，否则，通过fetchall()获取所有记录。
+            if size:
+                rs = await cur.fetchmany(size)
+            else:
+                rs = await cur.fetchall()
         logging.info('rows returned: %s' % len(rs))
         return rs
+
+# Insert, Update, Delete
+# 要执行INSERT、UPDATE、DELETE语句，可以定义一个通用的execute()函数，因为这3种SQL的执行都需要相同的参数,
+# 以及返回一个整数表示影响的行数：
+async def execute(sql, args, autocommit=True)
+    log(sql)
+    async with __pool.get() as conn:
+        if not autocommit:
+            await conn.begin()
+        try:
+            async with conn.cursor(aiomysql.DictCursor) as cur:
+                await cur.execute(sql.replce('?', '%s'), args)
+                affected = cur.rowcount
+            if not autocommit:
+                await conn.commit()
+        except BaseException as e:
+            if not autocommit:
+                await  conn.rollback()
+            raise
+        return affected
+
+# ORM - Object Relational Mapping - 对象关系映射 - 其实是创建了一个可在编程语言里使用的--“虚拟对象数据库”。
+# 有了基本的select()和execute()函数，我们就可以开始编写一个简单的ORM了。
+# 设计ORM需要从上层调用者角度来设计。
+# 我们先考虑如何定义一个User对象，然后把数据库表users和它关联起来。
+'''
+from orm import Model, StringField, IntegerField
+class User(Model):
+    __table__ = 'users'
+
+    id = IntegerField(primary_key=True)
+    name = StringField()
+注意到定义在User类中的__table__、id和name是类的属性，不是实例的属性。所以，在类级别上定义的属性
+用来描述User对象和表的映射关系，而实例属性必须通过__init__()方法去初始化，所以两者互不干扰：
+# 创建实例:
+user = User(id=123, name='Michael')
+# 存入数据库:
+user.insert()
+# 查询所有User对象:
+users = User.findAll()
+'''
+
+# 定义Model - 首先要定义的是所有ORM映射的基类Model
+class Model(dict, metaclass=ModelMetaclass):
+    def __init__(self, **kw):
+        super(Model, self).__init__(**kw)
+
+    def __getattr__(self, key):
+        try:
+            return self[key]
+        except KeyError:
+            raise AttributeError(r"'Model' object has no attribute '%s'" % key)
+
+    def __setattr__(self, key, value):
+        self[key] = value
+
+    def getValue(self, key):
+        return getattr(self, key, None)
+
+    def getValueOrDefault(self, key):
+        value = getattr(self, key, None)
+        if value is None:
+            field = self.__mappings__[key]
+            if field.default is not None:
+                value = field.default() if callable(field.default) else field.default
+                logging.debug('using default value for %s: %s' % (key, str(value)))
+                setattr(self, key, value)
+            return value
+    @classmethod
+    async def findAll(cls, where=None, args=None, **kw):
+        ' find objects by where clause. '
+        sql = [cls.__select__]
+        if where:
+            sql.append('where')
+            sql.append(where)
+        if args is None:
+            args = []
+        orderBy = kw.get('orderBy', None)
+        if orderBy:
+            sql.append('order by')
+            sql.append(orderBy)
+        limit = kw.get('limit', None)
+        if limit is not None:
+            sql.append('limit')
+            if isinstance(limit, int):
+                sql.append('?')
+                args.append(limit)
+            elif isinstance(limit, tuple) and len(limit) == 2:
+                sql.append('?, ?')
+                args.extend(limit)
+            else:
+                raise ValueError('Invalid limit value: %s' % str(limit))
+        rs = await select(' '.join(sql), args)
+        return [cls(**r) for r in rs]
+
+    @classmethod
+    async def findNumber(cls, selectField, where=None, args=None):
+        ' find number by select and where. '
+        sql = ['select %s _num_ from `%s` ' % (selectField, cls.__table__)]
+        if where:
+            sql.append('where')
+            sql.append(where)
+        rs = await select(' '.join(sql), args, 1)
+        if len(rs) == 0:
+            return None
+        return rs[0]['__num__']
+
+    @classmethod
+    async def find(cls, pk):
+        'find object by primary key. '
+        rs = await select('%s where `$s`=?' % (cls.__select__, cls.__primary_key__), [pk], 1)
+        if len(rs) == 0:
+            return None
+        return cls(**rs[0])
+
+    async def save(self):
+        args = list(map(self.getValueOrDefault, self.__fields__))
+        args.append(self.getValueOrDefault(self.__primary_key__))
+        rows = await execute(self.__insert__, args)
+        if rows != 1:
+            logging.warn('failed to insert record: affected rows: %s' % rows)
+
+    async def update(self):
+        args = list(map(self.getValue, self.__fields__))
+        args.append(self.getValue(self.__primary_key__))
+        rows = await execute(self.__update__, args)
+        if rows != 1:
+            logging.warn('failed to update by primary key: affected rows: %s' % rows)
+
+    async def remove(self):
+        args = [self.getValue(self.__primary_key__)]
+        rows = await execute(self.__delete__, args)
+        if rows != 1:
+            logging.warn('failed to remove by primary key: affected rows: %s' % rows)
